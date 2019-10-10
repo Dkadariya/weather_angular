0c41761f7558627701d36b09eeb121f84ba169e8
Dkadariya <dipeshk2015@gmail.com>
2019-10-10 11:25:02 -0400

    weather services by zip code

--- src/app/weather_services/weather-services.service.ts
+++ src/app/weather_services/weather-services.service.ts
@@ -6,7 +6,8 @@ import { stringify } from '@angular/compiler/src/util';
 @Injectable()
 export class WeatherServices {
     // private url:string= "http://api.openweathermap.org/data/2.5/weather";
-    private url: string = "http://api.openweathermap.org/data/2.5/box/city";
+    private state_url: string = "http://api.openweathermap.org/data/2.5/box/city";
+    private zip_url: string = "http://api.openweathermap.org/data/2.5/weather";
     constructor(private _http: HttpClient) { }
     getStateWeather() {
         // const params = new HttpParams().set('zip','45324').set('appid','dee1f31027f8331288a5bbe93c7ccf3f');
@@ -14,6 +15,11 @@ export class WeatherServices {
         // let url = "http://mocker.ennate.academy/users";
         // let AppId = 'dee1f31027f8331288a5bbe93c7ccf3f';
         // let qString= {"zip":45324,"APPID":AppId,"units":"metric"}
-        return this._http.get(this.url, { params });
+        return this._http.get(this.state_url, { params });
+    }
+
+    getZipWeather(){
+        const params = new HttpParams().set('zip', '45324').set('appid', 'dee1f31027f8331288a5bbe93c7ccf3f');
+        return this._http.get(this.zip_url, { params });
     }
 }
--- src/app/zip-weather/zip-weather.component.ts
+++ src/app/zip-weather/zip-weather.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { WeatherServices } from '../weather_services/weather-services.service';
 
 @Component({
   selector: 'app-zip-weather',
   templateUrl: './zip-weather.component.html',
   styleUrls: ['./zip-weather.component.css']
 })
-export class ZipWeatherComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class ZipWeatherComponent {
+  weather;
+  constructor(private weaterServices: WeatherServices) {
+    weaterServices.getZipWeather()
+    .subscribe(data => {this.weather = data; });
+   }
 
 }

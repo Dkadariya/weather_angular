ccd4ae97565fd9cfed0743d333a7455fb320d89e
Dkadariya <dipeshk2015@gmail.com>
2019-10-10 21:08:36 -0400

    minor updates and bug fix

--- src/app/weather_services/weather-services.service.ts
+++ src/app/weather_services/weather-services.service.ts
@@ -5,21 +5,17 @@ import { stringify } from '@angular/compiler/src/util';
 
 @Injectable()
 export class WeatherServices {
-    // private url:string= "http://api.openweathermap.org/data/2.5/weather";
-    private state_url: string = "http://api.openweathermap.org/data/2.5/box/city";
-    private zip_url: string = "http://api.openweathermap.org/data/2.5/weather";
+    private stateUrl = 'http://api.openweathermap.org/data/2.5/box/city';
+    private zipUrl = 'http://api.openweathermap.org/data/2.5/weather';
     constructor(private _http: HttpClient) { }
-    getStateWeather() {
+    getStateWeather(bbox: string) {
         // const params = new HttpParams().set('zip','45324').set('appid','dee1f31027f8331288a5bbe93c7ccf3f');
-        const params = new HttpParams().set('bbox', '-85,38,-80,41,10').set('appid', 'dee1f31027f8331288a5bbe93c7ccf3f');
-        // let url = "http://mocker.ennate.academy/users";
-        // let AppId = 'dee1f31027f8331288a5bbe93c7ccf3f';
-        // let qString= {"zip":45324,"APPID":AppId,"units":"metric"}
-        return this._http.get(this.state_url, { params });
+        const params = new HttpParams().set('bbox', bbox).set('appid', 'dee1f31027f8331288a5bbe93c7ccf3f');
+        return this._http.get(this.stateUrl, { params });
     }
 
-    getZipWeather(zip){
+    getZipWeather(zip: string) {
         const params = new HttpParams().set('zip', zip).set('appid', 'dee1f31027f8331288a5bbe93c7ccf3f');
-        return this._http.get(this.zip_url, { params });
+        return this._http.get(this.zipUrl, { params });
     }
 }
--- src/app/zip-weather/zip-weather.component.ts
+++ src/app/zip-weather/zip-weather.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { WeatherServices } from '../weather_services/weather-services.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-zip-weather',
@@ -7,51 +8,13 @@ import { WeatherServices } from '../weather_services/weather-services.service';
   styleUrls: ['./zip-weather.component.css']
 })
 export class ZipWeatherComponent {
-  weather: object = {
-    "coord": {"lon": -122.08,"lat": 37.39},
-    "weather": [
-      {
-        "id": 800,
-        "main": "Clear",
-        "description": "clear sky",
-        "icon": "01d"
-      }
-    ],
-    "base": "stations",
-    "main": {
-      "temp": 296.71,
-      "pressure": 1013,
-      "humidity": 53,
-      "temp_min": 294.82,
-      "temp_max": 298.71
-    },
-    "visibility": 16093,
-    "wind": {
-      "speed": 1.5,
-      "deg": 350
-    },
-    "clouds": {
-      "all": 1
-    },
-    "dt": 1560350645,
-    "sys": {
-      "type": 1,
-      "id": 5122,
-      "message": 0.0139,
-      "country": "US",
-      "sunrise": 1560343627,
-      "sunset": 1560396563
-    },
-    "timezone": -25200,
-    "id": 420006353,
-    "name": "Mountain View",
-    "cod": 200
-  };
+  fDate = new DatePipe('en-US').transform(Date.now(), 'short');
+  weather;
   zip: any;
   constructor(private weaterServices: WeatherServices) {
-    // weaterServices.getZipWeather('45324')
-    // .subscribe(data => {this.weather = data; },
-    //   error => {console.log(error); } );
+    weaterServices.getZipWeather('45324')
+    .subscribe(data => {this.weather = data; },
+      error => {console.log(error); } );
   }
 
    getWeather() {
@@ -59,5 +22,6 @@ export class ZipWeatherComponent {
     .subscribe(data => {this.weather = data; },
       error => {console.log(error); } );
    }
+
 
 }
